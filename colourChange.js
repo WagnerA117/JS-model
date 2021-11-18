1070f7bdcaea886552ede97e5316767cb7470911
WagnerA117 <77886511+WagnerA117@users.noreply.github.com>
2021-11-18 11:45:39 +0200

    working example

--- colourChange.js
+++ colourChange.js
@@ -1,8 +1,28 @@
-const textArea = document.getElementById("exampleID");
 
-const numExc = textArea.search(/[\!]/g);
 
-function checkExc(numExc){
+function checkExc(){
+    const textArea = document.getElementById("exampleInput");
 
-    console.log(numExc)
+    const numExc = textArea.value.split('').filter(i => i == '!').length
+
+    const finalText = document.getElementById("finalText")
+
+    if(numExc == 1){
+
+        finalText.style.color = "red";
+    }
+
+     else if(numExc == 2){
+        finalText.style.color = "blue"
+    }
+
+    else if(numExc == 3){
+        finalText.style.color = "green"
+    }
+    else{
+        finalText.style.color = "black"
+    };
+
+    finalText.value = textArea.value
 }
+
